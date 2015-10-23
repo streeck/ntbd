f8287530efba79ee3a83b74fb92f1752a3434450
Streeck <raverblazer@gmail.com>
2015-10-22 23:44:56 -0200

    Change key generation in table Tempo

--- date.py
+++ date.py
@@ -31,14 +31,17 @@ def writeData():
 def createSQL():
     queries = []
 
+    counter = 1
+
     start_date = date(2005, 2, 18)
     end_date = date(2015, 10, 21)
 
     for single_date in daterange(start_date, end_date):
         if single_date.weekday() in [5, 6]:
-            queries.append("INSERT INTO Tempo VALUES ({chave}, {diaNum}, '{diaSemana}', '{diaAbreviado}', {mesNum}, '{mesExtenso}', '{mesAbreviado}', {ano}, '{fds}')".format(chave=single_date.strftime("%Y%m%d"), diaNum=single_date.day, diaSemana=wkd[single_date.weekday()], diaAbreviado=wkdAbr[single_date.weekday()], mesNum=single_date.month, mesExtenso=mth[single_date.month-1], mesAbreviado=mthAbr[single_date.month-1], ano=single_date.year, fds='t'))
+            queries.append("INSERT INTO Tempo VALUES ({chave}, {diaNum}, '{diaSemana}', '{diaAbreviado}', {mesNum}, '{mesExtenso}', '{mesAbreviado}', {ano}, '{fds}')".format(chave=counter, diaNum=single_date.day, diaSemana=wkd[single_date.weekday()], diaAbreviado=wkdAbr[single_date.weekday()], mesNum=single_date.month, mesExtenso=mth[single_date.month-1], mesAbreviado=mthAbr[single_date.month-1], ano=single_date.year, fds='t'))
         else:
-            queries.append("INSERT INTO Tempo VALUES ({chave}, {diaNum}, '{diaSemana}', '{diaAbreviado}', {mesNum}, '{mesExtenso}', '{mesAbreviado}', {ano}, '{fds}')".format(chave=single_date.strftime("%Y%m%d"), diaNum=single_date.day, diaSemana=wkd[single_date.weekday()], diaAbreviado=wkdAbr[single_date.weekday()], mesNum=single_date.month, mesExtenso=mth[single_date.month-1], mesAbreviado=mthAbr[single_date.month-1], ano=single_date.year, fds='f'))
+            queries.append("INSERT INTO Tempo VALUES ({chave}, {diaNum}, '{diaSemana}', '{diaAbreviado}', {mesNum}, '{mesExtenso}', '{mesAbreviado}', {ano}, '{fds}')".format(chave=counter, diaNum=single_date.day, diaSemana=wkd[single_date.weekday()], diaAbreviado=wkdAbr[single_date.weekday()], mesNum=single_date.month, mesExtenso=mth[single_date.month-1], mesAbreviado=mthAbr[single_date.month-1], ano=single_date.year, fds='f'))
+        counter += 1
     return queries
 
 writeData()
