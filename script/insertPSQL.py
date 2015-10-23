07260900afb32c0ab56b18ea276cc57a4eeddf78
Streeck <raverblazer@gmail.com>
2015-10-23 03:20:18 -0200

    Add a bunch of stuff

--- script/insertPSQL.py
+++ script/insertPSQL.py
@@ -2,32 +2,64 @@
 
 # CODIGO QUE SEPARA O JSON E INSERE NO BANCO DE DADOS
 
+countries = ['BR', 'GB', 'US']
+
+chaves = {'US': 41, 'BR': 45, 'GB': 17}
+
 import re
 import json
 import psycopg2
+import datetime
+from datetime import datetime
+
+def getChaveTempo(cur, date):
+	cur.execute("SELECT chaveTempo FROM Tempo WHERE diaNum={dia} AND mesNum={mes} AND ano={ano}".format(dia=date.day, mes=date.month, ano=date.year))
+
+	key = cur.fetchall()
+	return key[0][0]
+
+def getChaveCanal(cur, channel_id):
+	cur.execute("SELECT chaveCanal FROM Canal WHERE canal='{}'".format(channel_id))
+
+	key = cur.fetchall()
+
+	return key[0][0]
+
 
 if __name__ == "__main__":
 	nameList = open('videos.json','r')
 	nameVideo = []
+
 	for a in json.loads(nameList.read()):
 		nameVideo.append(a[0])
 
 	try:
-		conn = psycopg2.connect("dbname='teste' user='postgres' host='localhost' ")
+		conn = psycopg2.connect(database="youtubiu", user="charles", password="asdf", host="127.0.0.1", port="5432")
 	except:
 		print "Nao foi possivel conectar no SGDB!"
 		exit()
-	
+
+	f = open('channel.json', 'r')
+	channel_id = json.loads(f.read())
+
 	cur = conn.cursor()
+
+	channel_id = getChaveCanal(cur, channel_id)
+
 	for video in nameVideo:
-		try:
-			videoList = open('data-video-'+video+'.json','r')
-			with videoList:
-				for value in json.loads(videoList.read())["rows"]:
-					cur.execute("INSERT INTO Video (chaveTempo,chavePerfil, chaveCanal, qtdViews, video, nome, qtdGostei, qtdNaoGostei) VALUES (%(chaveTempo)s, %(chavePerfil)s, %(chaveCanal)s, %(qtdViews)s, %(video)s,%(nome)s,%(qtdGostei)s,%(qtdNaoGostei)s);",{'chaveTempo':re.sub('-',"",value[0]), 'chavePerfil':hehehe1, 'chaveCanal':hehehe2, 'qtdViews':value[1], 'video':video,'nome':hehehe3, 'qtdGostei': value[2], 'qtdNaoGostei': value[3]})
-				conn.commit()
-				print "Video assimilado no banco de dados..."+a
-		except IOError:
-			print "Video provavelmente privado..."+a
+		for country in countries:
+			try:
+				videoList = open('data-video-{id};country=={code}.json'.format(id=video, code=country),'r')
+				print videoList
+				with videoList:
+					for value in json.loads(videoList.read())["rows"]:
+						date = datetime.strptime(value[0], "%Y-%m-%d")
+						cur.execute("INSERT INTO Video VALUES ({chaveTempo}, {chavePerfil}, {chaveCanal}, {qtdViews}, '{video}', '{nome}', {qtdGostei}, {qtdNaoGostei});".format(chaveTempo=getChaveTempo(cur, date), chavePerfil=chaves[country], chaveCanal=channel_id, qtdViews=value[1], video=video, nome='teste', qtdGostei=value[2], qtdNaoGostei=value[3]))
+
+						# cur.execute("INSERT INTO Video VALUES (%(chaveTempo)s, %(chavePerfil)s, %(chaveCanal)s, %(qtdViews)s, %(video)s,%(nome)s,%(qtdGostei)s,%(qtdNaoGostei)s);",{'chaveTempo':re.sub('-',"",value[0]), 'chavePerfil':hehehe1, 'chaveCanal':hehehe2, 'qtdViews':value[1], 'video':video,'nome':hehehe3, 'qtdGostei': value[2], 'qtdNaoGostei': value[3]})
+					conn.commit()
+					print "Video assimilado no banco de dados..."+video
+			except IOError:
+				print "Video provavelmente privado..."+video
 	cur.close()
-	conn.close()		
+	conn.close()
