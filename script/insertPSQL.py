7f6c9b1d28948f8aec49ba69f641419d5141f7c5
Streeck <raverblazer@gmail.com>
2015-10-23 03:49:16 -0200

    Working

--- script/insertPSQL.py
+++ script/insertPSQL.py
@@ -2,7 +2,7 @@
 
 # CODIGO QUE SEPARA O JSON E INSERE NO BANCO DE DADOS
 
-countries = ['BR', 'GB', 'US']
+countries = ['GB', 'US', 'BR']
 
 chaves = {'US': 41, 'BR': 45, 'GB': 17}
 
@@ -50,15 +50,18 @@ if __name__ == "__main__":
 		for country in countries:
 			try:
 				videoList = open('data-video-{id};country=={code}.json'.format(id=video, code=country),'r')
-				print videoList
-				with videoList:
-					for value in json.loads(videoList.read())["rows"]:
-						date = datetime.strptime(value[0], "%Y-%m-%d")
-						cur.execute("INSERT INTO Video VALUES ({chaveTempo}, {chavePerfil}, {chaveCanal}, {qtdViews}, '{video}', '{nome}', {qtdGostei}, {qtdNaoGostei});".format(chaveTempo=getChaveTempo(cur, date), chavePerfil=chaves[country], chaveCanal=channel_id, qtdViews=value[1], video=video, nome='teste', qtdGostei=value[2], qtdNaoGostei=value[3]))
 
-						# cur.execute("INSERT INTO Video VALUES (%(chaveTempo)s, %(chavePerfil)s, %(chaveCanal)s, %(qtdViews)s, %(video)s,%(nome)s,%(qtdGostei)s,%(qtdNaoGostei)s);",{'chaveTempo':re.sub('-',"",value[0]), 'chavePerfil':hehehe1, 'chaveCanal':hehehe2, 'qtdViews':value[1], 'video':video,'nome':hehehe3, 'qtdGostei': value[2], 'qtdNaoGostei': value[3]})
-					conn.commit()
-					print "Video assimilado no banco de dados..."+video
+				jasao = json.loads(videoList.read())
+
+				with videoList:
+					if 'rows' in jasao:
+						for value in jasao["rows"]:
+							date = datetime.strptime(value[0], "%Y-%m-%d")
+							cur.execute("INSERT INTO Video VALUES ({chaveTempo}, {chavePerfil}, {chaveCanal}, {qtdViews}, '{video}', '{nome}', {qtdGostei}, {qtdNaoGostei});".format(chaveTempo=getChaveTempo(cur, date), chavePerfil=chaves[country], chaveCanal=channel_id, qtdViews=value[1], video=video, nome='teste', qtdGostei=value[2], qtdNaoGostei=value[3]))
+
+							# cur.execute("INSERT INTO Video VALUES (%(chaveTempo)s, %(chavePerfil)s, %(chaveCanal)s, %(qtdViews)s, %(video)s,%(nome)s,%(qtdGostei)s,%(qtdNaoGostei)s);",{'chaveTempo':re.sub('-',"",value[0]), 'chavePerfil':hehehe1, 'chaveCanal':hehehe2, 'qtdViews':value[1], 'video':video,'nome':hehehe3, 'qtdGostei': value[2], 'qtdNaoGostei': value[3]})
+						conn.commit()
+						print "Video assimilado no banco de dados..."+video
 			except IOError:
 				print "Video provavelmente privado..."+video
 	cur.close()
