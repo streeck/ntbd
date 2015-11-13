6c3a2d26b7837ea8e4a92f91f0a3b692144020ec
Streeck <raverblazer@gmail.com>
2015-11-12 23:39:43 -0200

    Add extract 1b and change 1a

--- agregados.sql
+++ agregados.sql
@@ -4,10 +4,23 @@ CREATE TABLE agregado1a (
     categoria varchar NOT NULL,
     continente varchar NOT NULL,
     pais varchar NOT NULL,
-    estado varchar DEFAULT 'undefined',
+    ano int NOT NULL,
+    sexo char
+);
+
+CREATE TABLE agregado1b (
+    chave serial PRIMARY KEY,
+    qtdViews int NOT NULL,
+    categoria varchar NOT NULL,
+    continente varchar NOT NULL,
+    pais varchar NOT NULL,
     diaNum int NOT NULL,
     diaSemana varchar NOT NULL,
+    diaAbreviado varchar NOT NULL,
     mesNum int NOT NULL,
+    mesExtenso varchar NOT NULL,
+    mesAbreviado varchar NOT NULL,
     ano int NOT NULL,
-    fds boolean NOT NULL
+    fds boolean NOT NULL,
+    sexo char,
 );
